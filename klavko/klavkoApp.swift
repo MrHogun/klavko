8eb746458d32a71af354ddf3c0285de8a0ba0b24
MrHogun <s.j.000abk@gmail.com>
2026-04-28 18:46:46 +0300

    Initial Commit

---
+++ klavko/klavkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  klavkoApp.swift
+//  klavko
+//
+//  Created by Petro Halkevych on 28.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct klavkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
